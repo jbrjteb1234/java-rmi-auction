f93cbf46d45899cb59e3092318ce4382c587280f
jbrjteb1234 <james@bright.me.uk>
2024-10-15 09:46:24 +0100

    Constructor and getters for AuctionItem

--- AuctionItem.java
+++ AuctionItem.java
@@ -1,3 +1,27 @@
-public class AuctionItem {
+import javax.crypto.SealedObject;
+import java.io.Serializable;
+
+public class AuctionItem implements Serializable{
+    private int itemID;
+    private String name;
+    private String description;
+    private int highestBid;
+
+    public AuctionItem(int itemID, String name, String description) {
+        this.itemID = itemID;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getItemID(){
+        return itemID;
+    }
+
+    public String getName(){ 
+        return name; 
+    }
     
+    public String getDescription() { 
+        return description; 
+    }
 }
