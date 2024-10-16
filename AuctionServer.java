d783db583e802a41f1941f1f59b31d8b5ad54a37
jbrjteb1234 <james@bright.me.uk>
2024-10-16 01:47:08 +0100

    test items creation/storage and loading the key to the server

--- AuctionServer.java
+++ AuctionServer.java
@@ -1,11 +1,43 @@
 import java.net.MalformedURLException;
 import java.rmi.*;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
 
+import javax.crypto.SecretKey;
 import javax.crypto.Cipher;
 import javax.crypto.SealedObject;
 
-public class AuctionServer implements Auction{ 
+public class AuctionServer extends UnicastRemoteObject implements Auction{
+
+    private HashMap<Integer,AuctionItem> items;
+    private SecretKey key;
+
+    public AuctionServer() throws Exception {
+        super(); //initates uro, binding to ports  etc. needed for RMI to function
+        this.items = new HashMap<>();
+
+        try {
+            this.key = KeyManager.loadKey();
+        } catch (Exception e){
+            try{
+                this.key = KeyManager.generateKey();
+            }catch (Exception e2){
+                System.out.println("Error generating key");
+            }
+        }
+
+
+        //test items to laod into the server
+        AuctionItem test1 = new AuctionItem(10, "car", "bmw");
+        AuctionItem test2 = new AuctionItem(20, "watch", "rolex");
+        AuctionItem test3 = new AuctionItem(30, "plane", "airbus a380");
+
+        this.items.put(test1.getItemID(), test1);
+        this.items.put(test2.getItemID(), test2);
+        this.items.put(test3.getItemID(), test3);
+
+        System.out.println("Auction server instantiated");
+    }   
 
     public SealedObject getSpec(int itemID) throws RemoteException {
         //TODO: IMPLEMENT
@@ -15,7 +47,7 @@ public class AuctionServer implements Auction{
     public static void main(String[] args){
         try{
             AuctionServer server  = new AuctionServer();    //new server
-            Naming.rebind("Auction", server);               //binds server to Auction
+            Naming.rebind("//localhost/Auction", server);               //binds server to Auction
         }catch (Exception e) {
             System.out.println("Error creating server\n");
         }
