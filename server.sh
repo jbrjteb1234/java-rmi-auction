e917c0f6ddaf4cd8a3bcb033548aa43a63c93a24
jbrjteb1234 <james@bright.me.uk>
2024-10-17 12:05:14 +0100

    Added testKey file manager to shell file for server

--- server.sh
+++ server.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Define the file path
+FILE="keys/testKeys.aes"
+
+# Check if the file exists
+if [ ! -f "$FILE" ]; then
+    mkdir -p "keys"  
+    touch "$FILE"     
+    echo "$FILE created."
+fi
+
 rmiregistry &
 sleep 2
 java AuctionServer
