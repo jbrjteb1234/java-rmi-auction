bb39663406d529ccccf09f32784f4e34267fe3ac
jbrjteb1234 <james@bright.me.uk>
2024-11-14 05:28:30 +0000

    Set up private/public key generation

--- KeyManager.java
+++ KeyManager.java
@@ -1,48 +1,59 @@
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-import javax.crypto.KeyGenerator;
+import java.io.*;
+import java.security.*;
+import java.security.spec.*;
 
 public class KeyManager {
-    public static SecretKey loadKey() throws Exception {
-        try{
-            return loadKeyRoutine();
-        } catch (Exception e){
-            try{
-                return generateKey();
-            } catch (Exception e1){
-                System.out.println("Error generating new key");
-                return null;
-            }
+
+    private static final String RSA_PUBLIC_KEY_PATH = "keys/server_public.key";
+    private static final String RSA_PRIVATE_KEY_PATH = "keys/server_private.key";
+
+    public static KeyPair loadServerKeyPair() throws Exception {
+        try {
+            return loadRSAKeyPair();
+        } catch (Exception e) {
+            System.out.println("Generating new RSA key pair...");
+            return generateRSAKeyPair();
         }
     }
 
-    private static SecretKey loadKeyRoutine() throws Exception {
-        try (FileInputStream file = new FileInputStream("keys/testKey.aes")){
-            //deserialize the input from the file
-            byte[] keyBytes = new byte[16];
-            if (file.read(keyBytes) != keyBytes.length) {
-                throw new IOException("No keys found");
-            }
-            return new SecretKeySpec(keyBytes, "AES");
+    private static KeyPair loadRSAKeyPair() throws Exception {
+        PrivateKey privateKey;
+        PublicKey publicKey;
+
+        // load private key
+        try (FileInputStream fis = new FileInputStream(RSA_PRIVATE_KEY_PATH)) {
+            byte[] privateKeyBytes = fis.readAllBytes();
+            PKCS8EncodedKeySpec privateSpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            privateKey = keyFactory.generatePrivate(privateSpec);
         }
+
+        // load public key
+        try (FileInputStream fis = new FileInputStream(RSA_PUBLIC_KEY_PATH)) {
+            byte[] publicKeyBytes = fis.readAllBytes();
+            X509EncodedKeySpec publicSpec = new X509EncodedKeySpec(publicKeyBytes);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            publicKey = keyFactory.generatePublic(publicSpec);
+        }
+
+        return new KeyPair(publicKey, privateKey);
     }
 
-    public static SecretKey generateKey() throws Exception {
-        KeyGenerator gen = KeyGenerator.getInstance("AES");
-        gen.init(128);
+    public static KeyPair generateRSAKeyPair() throws Exception {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        KeyPair keyPair = keyGen.generateKeyPair();
 
-        SecretKey newKey = gen.generateKey();
-
-        try (FileOutputStream fos = new FileOutputStream("keys/testKey.aes")){
-            fos.write(newKey.getEncoded());
+        // save private key
+        try (FileOutputStream fos = new FileOutputStream(RSA_PRIVATE_KEY_PATH)) {
+            fos.write(keyPair.getPrivate().getEncoded());
         }
 
+        // save public key
+        try (FileOutputStream fos = new FileOutputStream(RSA_PUBLIC_KEY_PATH)) {
+            fos.write(keyPair.getPublic().getEncoded());
+        }
 
-        return newKey;
+        return keyPair;
     }
 }
